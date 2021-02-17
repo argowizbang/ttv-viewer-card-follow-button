477976a1ece6c9a0ea76ea110a28e7c6b6351b23
ArgoWizbang <argowizbang@gmail.com>
2021-02-17 03:10:08 -0500

    Reverting from breaking update in 0.7.0

--- vendor/browser-polyfill.js
+++ vendor/browser-polyfill.js
@@ -11,7 +11,11 @@
     global.browser = mod.exports;
   }
 })(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function (module) {
+<<<<<<< HEAD
   /* webextension-polyfill - v0.7.0 - Tue Nov 10 2020 20:24:04 */
+=======
+  /* webextension-polyfill - v0.6.0 - Mon Dec 23 2019 12:32:53 */
+>>>>>>> parent of bc729d2 (Update Mozilla's webextension-polyfill to v0.7.0)
 
   /* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
 
@@ -252,12 +256,15 @@
               "minArgs": 3,
               "maxArgs": 3,
               "singleCallbackArg": true
+<<<<<<< HEAD
             },
             "elements": {
               "createSidebarPane": {
                 "minArgs": 1,
                 "maxArgs": 1
               }
+=======
+>>>>>>> parent of bc729d2 (Update Mozilla's webextension-polyfill to v0.7.0)
             }
           }
         },
@@ -600,6 +607,7 @@
             "minArgs": 0,
             "maxArgs": 1
           },
+<<<<<<< HEAD
           "goBack": {
             "minArgs": 0,
             "maxArgs": 1
@@ -608,6 +616,8 @@
             "minArgs": 0,
             "maxArgs": 1
           },
+=======
+>>>>>>> parent of bc729d2 (Update Mozilla's webextension-polyfill to v0.7.0)
           "highlight": {
             "minArgs": 1,
             "maxArgs": 1
